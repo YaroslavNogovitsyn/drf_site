3951351d4b307c6eeeb6aeef51bc85f5594d795e
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-05-09 20:00:36 +0300

    added get, post, put, delete methods

--- women/views.py
+++ women/views.py
@@ -1,9 +1,51 @@
+from django.forms import model_to_dict
 from rest_framework import generics
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from women.models import Women
-from women.serializers import WomenSerializer
+import women
+from .models import Women
+from .serializers import WomenSerializer
 
 
-class WomenApiView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+class WomenAPIView(APIView):
+    def get(self, request):
+        w = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many=True).data})
+
+    def post(self, request):
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'post': serializer.data})
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'error': 'Method PUT not allowed'})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({'error': 'Objects does not exists'})
+
+        serializer = WomenSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'post': serializer.data})
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'error': "Method delete not allowed"})
+        try:
+            record = Women.objects.filter(pk=pk)
+            record.delete()
+        except:
+            return Response({'error': "Object does not exist"})
+
+        return Response({"post": "delete post " + str(pk)})
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
